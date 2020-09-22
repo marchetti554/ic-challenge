68f1a63cf2b436d28a18cd7d1373fa7fef1f3f85
Marco Marchetti <marchetti554@gmail.com>
2020-09-22 13:27:14 -0300

    Completed Swagger documentation.

--- src/main/java/icchallenge/challengeapi/controller/ClientController.java
+++ src/main/java/icchallenge/challengeapi/controller/ClientController.java
@@ -6,6 +6,7 @@ import icchallenge.challengeapi.data.request.CreateClientRequest;
 
 import icchallenge.challengeapi.service.ClientService;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,16 +27,23 @@ public class ClientController {
     @Autowired
     private ClientService clientService;
 
+    @ApiOperation(value = "Calculates the client KPI.",
+            notes = "Returns the average age and standard deviation.",
+            response = KpiData.class)
     @GetMapping("/kpideclientes")
     public KpiData getClientsKpi() {
         return clientService.calculateClientsKpi();
     }
 
+    @ApiOperation(value = "Retrieves all clients and calculates their estimated death date.",
+            responseContainer = "Map")
     @GetMapping("/listclientes")
     public Map<Long, Client> getClients() {
         return clientService.listClientsWithEstimatedDeathDate();
     }
 
+    @ApiOperation(value = "Creates a new Client in the database.",
+            notes = "The Client age is calculated automatically.")
     @PostMapping("/creacliente")
     public void createClient(@RequestBody CreateClientRequest clientToCreate) {
         clientService.saveClient(clientToCreate.toModel());
